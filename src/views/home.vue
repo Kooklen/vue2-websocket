6636a3a84005de1c5803ff7b847803e3c67a0444
‘zhandehuang’ <859540029@qq.com>
2022-03-25 17:30:48 +0800

    1.1

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,23 @@
 <template>
 <div>
-  123
+  <div>
+    <input type="text" placeholder="请输入消息" v-model="msg">
+    <button @click="handleSendBtnClick">发送</button>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data(){
+    return{
+      msg:"",
+      username:"",
+      msgList:[]
+    }
+  },
+  methods:{}
 }
 </script>
 
