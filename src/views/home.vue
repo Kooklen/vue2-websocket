d138cb540c5f5c87812acc60fe6036dfe374304d
‘zhandehuang’ <859540029@qq.com>
2022-03-28 15:30:03 +0800

    1.3

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,20 @@
 <template>
 <div>
   <div>
+    <ul>
+      <li
+      v-for="item of msgList"
+      :key="item.id"
+      >
+        <p>
+          <span>{{item.user}}</span>
+          <span>{{new Date(item.dateTime) }}</span>
+        </p>
+        <p>
+          消息：<span>{{item.msg}}</span>
+        </p>
+      </li>
+    </ul>
     <input type="text" placeholder="请输入消息" v-model="msg">
     <button @click="handleSendBtnClick">发送</button>
   </div>
@@ -17,7 +31,10 @@ export default {
       msgList:[]
     }
   },
-  methods:{}
+  methods:{},
+  mounted() {
+    this.username = localStorage;
+  }
 }
 </script>
 
